0eefda584d4aa71f44d66e916df174c7a254212b
Evgeny Stepanov <evge.stepanov@mail.ru>
2024-09-25 23:13:12 +0500

    Work on auth page 1.8

--- src/auth/js/auth-animation.js
+++ src/auth/js/auth-animation.js
@@ -1,3 +1,21 @@
 //* Animation on the input icon color when focused
 
 //* Animation of text "Clients" when hovering over a button
+function changeColorClientsText() {
+	const formButton = document.querySelectorAll(".form button"),
+		clientsText = document.querySelector(".auth__title span");
+
+	formButton.forEach(btn => {
+		btn.addEventListener("mouseover", () => {
+			clientsText.classList.add("is-hover");
+		});
+	});
+
+	formButton.forEach(btn => {
+		btn.addEventListener("mouseout", () => {
+			clientsText.classList.remove("is-hover");
+		});
+	});
+}
+
+changeColorClientsText();
