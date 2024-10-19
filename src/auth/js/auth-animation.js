10032fa567fe693787ce9785a74d5fe537ad1cef
Evgeny Stepanov <evge.stepanov@mail.ru>
2024-10-19 21:52:16 +0500

    Work on auth page 2.2

--- src/auth/js/auth-animation.js
+++ src/auth/js/auth-animation.js
@@ -1,63 +1,55 @@
+//* Animation on the input icon color when focused
+function changeInputIconColor() {
+	const inputParents = document.querySelectorAll("p[data-login]"),
+		inputs = document.querySelectorAll("p[data-login] input");
+
+	inputs.forEach((input, index) => {
+		input.addEventListener("focus", () => {
+			inputParents[index].querySelector("span").classList.add("is-focus");
+		});
+
+		input.addEventListener("blur", () => {
+			inputParents[index].querySelector("span").classList.remove("is-focus");
+		});
+	});
+}
+
 //* Toggle password visibility and change the icon
-function togglePassword() {
-	const passInput = document.querySelector("input#password"),
-		svgIconParent = document.querySelector("input#password ~ span"),
-		svgIcon = svgIconParent.querySelector("svg");
+function toggleTypePasswordAndChangeIcon() {
+	const inputPassword = document.querySelector("input#password"),
+		iconPasswordParent = document.querySelector("input#password ~ span"),
+		iconCloseEye = document.querySelector("svg[data-eye='close']"),
+		iconOpenEye = document.querySelector("svg[data-eye='open']");
 
-	svgIconParent.addEventListener("click", () => {
-		//! Don't work, but worked innerHTML
-		let newSvgIcon = `
-			<?xml version="1.0" encoding="utf-8"?>
-			<svg width="30px" height="30px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path fill-rule="evenodd" clip-rule="evenodd" d="M0 8L3.07945 4.30466C4.29638 2.84434 6.09909 2 8 2C9.90091 2 11.7036 2.84434 12.9206 4.30466L16 8L12.9206 11.6953C11.7036 13.1557 9.90091 14 8 14C6.09909 14 4.29638 13.1557 3.07945 11.6953L0 8ZM8 11C9.65685 11 11 9.65685 11 8C11 6.34315 9.65685 5 8 5C6.34315 5 5 6.34315 5 8C5 9.65685 6.34315 11 8 11Z" fill="#000000"/>
-			</svg>
-		`;
-
-		if (passInput.type === "password") {
-			passInput.type = "text";
-			svgIconParent.insertAdjacentHTML("afterbegin", newSvgIcon);
-			svgIcon.style.display = "none";
-		} else if (passInput.type === "text") {
-			passInput.type = "password";
-			svgIcon.style.display = "inline";
+	iconPasswordParent.addEventListener("click", () => {
+		if (inputPassword.type === "password") {
+			inputPassword.type = "text";
+			iconCloseEye.classList.remove("is-visible");
+			iconOpenEye.classList.add("is-visible");
+		} else {
+			inputPassword.type = "password";
+			iconCloseEye.classList.add("is-visible");
+			iconOpenEye.classList.remove("is-visible");
 		}
 	});
 }
 
-//* Animation on the input icon color when focused
-function changeColorInputSvg() {
-	const inputs = document.querySelectorAll("input[data-focus]"),
-		svgIcons = document.querySelectorAll("input[data-focus] ~ span svg");
-
-	inputs.forEach((input, i) => {
-		input.addEventListener("focus", () => {
-			svgIcons[i].classList.add("is-focus");
-		});
-
-		input.addEventListener("blur", () => {
-			svgIcons[i].classList.remove("is-focus");
-		});
-	});
-}
-
 //* Animation of text "Clients" when hovering over a button
-function changeColorClientsText() {
-	const formButton = document.querySelectorAll(".form button"),
+function changeClientsTextColor() {
+	const formButtons = document.querySelectorAll(".form button"),
 		clientsText = document.querySelector(".auth__title span");
 
-	formButton.forEach(btn => {
+	formButtons.forEach(btn => {
 		btn.addEventListener("mouseover", () => {
 			clientsText.classList.add("is-hover");
 		});
-	});
 
-	formButton.forEach(btn => {
 		btn.addEventListener("mouseout", () => {
 			clientsText.classList.remove("is-hover");
 		});
 	});
 }
 
-togglePassword();
-changeColorInputSvg();
-changeColorClientsText();
+changeInputIconColor();
+toggleTypePasswordAndChangeIcon();
+changeClientsTextColor();
