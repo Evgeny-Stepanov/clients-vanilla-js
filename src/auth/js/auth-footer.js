e8a55ef90db60792e020d1d1c5a8efe338712515
Evgeny Stepanov <evge.stepanov@mail.ru>
2024-11-11 20:08:15 +0500

    Work on auth page 2.6

--- src/auth/js/auth-footer.js
+++ src/auth/js/auth-footer.js
@@ -1,10 +1,30 @@
 function changeInputRangeValue() {
 	const input = document.querySelector("#fz"),
-		span = document.querySelector(".settings__input-range-value");
+		span = document.querySelector(".settings__font-size-value");
 
 	input.addEventListener("input", () => {
-		span.textContent = `${input.value} px`;
+		span.textContent = input.value;
+	});
+}
+
+function showSettingsBlock() {
+	const btn = document.querySelector(".footer__settings-btn"),
+		settingsBlock = document.querySelector(".footer__settings");
+
+	btn.addEventListener("click", () => {
+		if (!settingsBlock.classList.contains("is-open")) {
+			settingsBlock.classList.add("is-open");
+			settingsBlock.classList.add("fade-in");
+			settingsBlock.classList.remove("fade-out");
+		} else {
+			settingsBlock.classList.remove("fade-in");
+			settingsBlock.classList.add("fade-out");
+			setTimeout(() => {
+				settingsBlock.classList.remove("is-open");
+			}, 300);
+		}
 	});
 }
 
 changeInputRangeValue();
+showSettingsBlock();
